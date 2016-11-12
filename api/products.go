3a50ff2d6cb855c924a5dde16a6461bd51ca6d70
DennisDenuto <dennis.leon39@gmail.com>
2016-11-12 15:25:44 -0800

    Add ListProducts api

--- api/products.go
+++ api/products.go
@@ -10,6 +10,7 @@ import (
 
 type ProductsInfo interface {
 	GetProduct(productId string) (Product, error)
+	ListProducts(lat string, lon string) (ProductList, error)
 }
 
 type Products struct {
@@ -19,9 +20,28 @@ type Products struct {
 func NewProducts(serverToken string) Products {
 	return Products{
 		ServerTokenClient: client.ServerTokenClient{
-			serverToken: serverToken,
+			ServerToken: serverToken,
 		},
 	}
+}
+
+func (p Products) ListProducts(lat string, lon string) (ProductList, error) {
+	resp, err := p.ServerTokenClient.Get(fmt.Sprintf("v1/products"), nil)
+
+	if err != nil {
+		return ProductList{}, errors.Wrap(err, "Unable to get Products from uber")
+	}
+
+	uberProducts := ProductList{}
+	uberResp, _ := ioutil.ReadAll(resp)
+
+	err = json.Unmarshal(uberResp, &uberProducts)
+
+	if err != nil {
+		return ProductList{}, errors.Wrap(err, "Unable to parse Products response from uber")
+	}
+
+	return uberProducts, nil
 }
 
 func (p Products) GetProduct(productId string) (Product, error) {
@@ -40,6 +60,10 @@ func (p Products) GetProduct(productId string) (Product, error) {
 	}
 
 	return uberProduct, nil
+}
+
+type ProductList struct {
+	Products []Product `json:"products"`
 }
 
 type Product struct {
@@ -63,7 +87,7 @@ type PriceDetails struct {
 
 	CostPerMin      float64 `json:"cost_per_minute"`
 
-	ServiceFees     []float64 `json:"service_fees"`
+	ServiceFees     []ServiceFee `json:"service_fees"`
 
 	Minimum         float64 `json:"minimum"`
 
@@ -74,4 +98,9 @@ type PriceDetails struct {
 	CancellationFee float64 `json:"cancellation_fee"`
 
 	CurrencyCode    string `json:"currency_code"`
+}
+
+type ServiceFee struct {
+	Fee float64 `json:"fee"`
+	Name string `json:"name"`
 }
--- api/products_test.go
+++ api/products_test.go
@@ -13,15 +13,16 @@ import (
 
 var _ = Describe("Products", func() {
 
-	Context("User has provided valid server token", func() {
-		var client ProductsInfo
-		var fakeServerTokenClient *clientfakes.FakeClient
+	Describe("GetProduct", func() {
+		Context("User has provided valid server token", func() {
+			var client ProductsInfo
+			var fakeServerTokenClient *clientfakes.FakeClient
 
-		Context("Uber service returns valid successful response", func() {
-			BeforeEach(func() {
-				fakeServerTokenClient = &clientfakes.FakeClient{}
+			Context("Uber service returns valid successful response", func() {
+				BeforeEach(func() {
+					fakeServerTokenClient = &clientfakes.FakeClient{}
 
-				fakeServerTokenClient.GetReturns(strings.NewReader(`{
+					fakeServerTokenClient.GetReturns(strings.NewReader(`{
 										   "capacity": 4,
 										   "description": "The original Uber",
 										   "price_details": {
@@ -39,69 +40,182 @@ var _ = Describe("Products", func() {
 										   "product_id": "d4abaae7-f4d6-4152-91cc-77523e8165a4",
 										   "shared": false
 										}`), nil)
-				client = Products{
-					ServerTokenClient: fakeServerTokenClient,
-				}
+					client = Products{
+						ServerTokenClient: fakeServerTokenClient,
+					}
+				})
+
+				It("should generate valid time estimate returned by uber", func() {
+					resp, err := client.GetProduct("123")
+
+					Expect(err).ToNot(HaveOccurred())
+					Expect(resp).ToNot(BeNil())
+
+					Expect(fakeServerTokenClient).
+					To(HaveReceived("Get").
+					With(Equal("v1/products/123")).AndWith(BeNil()))
+
+					Expect(resp.PriceDetails.DistanceUnit).To(Equal("mile"))
+					Expect(resp.Description).To(Equal("The original Uber"))
+					Expect(resp.Capacity).To(Equal(4))
+					Expect(resp.Image).To(Equal("http: //d1a3f4spazzrp4.cloudfront.net/car.jpg"))
+					Expect(resp.Shared).To(Equal(false))
+				})
+
+				Context("Invalid json format", func() {
+					BeforeEach(func() {
+						fakeServerTokenClient = &clientfakes.FakeClient{}
+						fakeServerTokenClient.GetReturns(strings.NewReader("NOT VALID JSON"), nil)
+
+						client = Products{
+							ServerTokenClient: fakeServerTokenClient,
+						}
+					})
+
+					It("Should return an error", func() {
+						_, err := client.GetProduct("321")
+
+						Expect(err).To(HaveOccurred())
+						Expect(err).To(MatchError("Unable to parse Product response from uber: invalid character 'N' looking for beginning of value"))
+					})
+				})
 			})
 
-			It("should generate valid time estimate returned by uber", func() {
-				resp, err := client.GetProduct("123")
+			Context("Uber service returns non-successful response", func() {
+				var uberError error
 
-				Expect(err).ToNot(HaveOccurred())
-				Expect(resp).ToNot(BeNil())
-
-				Expect(fakeServerTokenClient).
-				To(HaveReceived("Get").
-				With(Equal("v1/products/123")).AndWith(BeNil()))
-
-
-				Expect(resp.PriceDetails.DistanceUnit).To(Equal("mile"))
-				Expect(resp.Description).To(Equal("The original Uber"))
-				Expect(resp.Capacity).To(Equal(4))
-				Expect(resp.Image).To(Equal("http: //d1a3f4spazzrp4.cloudfront.net/car.jpg"))
-				Expect(resp.Shared).To(Equal(false))
-			})
-
-			Context("Invalid json format", func() {
 				BeforeEach(func() {
+					uberError = fmt.Errorf("Some random error occurred!")
+
 					fakeServerTokenClient = &clientfakes.FakeClient{}
-					fakeServerTokenClient.GetReturns(strings.NewReader("NOT VALID JSON"), nil)
+					fakeServerTokenClient.GetReturns(nil, uberError)
 
 					client = Products{
 						ServerTokenClient: fakeServerTokenClient,
 					}
 				})
 
-				It("Should return an error", func() {
+				It("should return error message returned by uber", func() {
 					_, err := client.GetProduct("321")
 
 					Expect(err).To(HaveOccurred())
-					Expect(err).To(MatchError("Unable to parse Product response from uber: invalid character 'N' looking for beginning of value"))
+					Expect(err).To(MatchError("Unable to get Product Info from uber: Some random error occurred!"))
 				})
 			})
+
 		})
+	})
 
-		Context("Uber service returns non-successful response", func() {
-			var uberError error
+	Describe("ListProducts", func() {
+		Context("User has provided valid server token", func() {
+			var client ProductsInfo
+			var fakeServerTokenClient *clientfakes.FakeClient
 
-			BeforeEach(func() {
-				uberError = fmt.Errorf("Some random error occurred!")
+			Context("Uber service returns valid successful response", func() {
+				BeforeEach(func() {
+					fakeServerTokenClient = &clientfakes.FakeClient{}
 
-				fakeServerTokenClient = &clientfakes.FakeClient{}
-				fakeServerTokenClient.GetReturns(nil, uberError)
+					fakeServerTokenClient.GetReturns(strings.NewReader(`{
+											  "products": [
+											    {
+											       "capacity": 2,
+											       "description": "Ride for less with uberPOOL",
+											       "short_description": "POOL",
+											       "price_details": null,
+											       "cash_enabled": false,
+											       "image": "http://d1a3f4spazzrp4.cloudfront.net/car.jpg",
+											       "display_name": "POOL",
+											       "product_id": "26546650-e557-4a7b-86e7-6a3942445247",
+											       "shared": true
+											    },
+											    {
+											       "capacity": 4,
+											       "description": "The low-cost Uber",
+											       "short_description": "uberX",
+											       "price_details": {
+												  "distance_unit": "mile",
+												  "cost_per_minute": 0.26,
+												  "service_fees": [
+												     {
+													"fee": 1.0,
+													"name": "Safe Rides Fee"
+												     }
+												  ],
+												  "minimum": 5.0,
+												  "cost_per_distance": 1.3,
+												  "base": 2.2,
+												  "cancellation_fee": 5.0,
+												  "currency_code": "USD"
+											       },
+											       "cash_enabled": false,
+											       "image": "http://d1a3f4spazzrp4.cloudfront.net/car.jpg",
+											       "display_name": "uberX",
+											       "product_id": "a1111c8c-c720-46c3-8534-2fcdd730040d",
+											       "shared": false
+											    }
+											  ]
+											}`), nil)
+					client = Products{
+						ServerTokenClient: fakeServerTokenClient,
+					}
+				})
 
-				client = Products{
-					ServerTokenClient: fakeServerTokenClient,
-				}
+				It("should generate valid time estimate returned by uber", func() {
+					resp, err := client.ListProducts("123", "321")
+
+					Expect(err).ToNot(HaveOccurred())
+					Expect(resp).ToNot(BeNil())
+
+					Expect(fakeServerTokenClient).
+					To(HaveReceived("Get").
+					With(Equal("v1/products")).AndWith(BeNil()))
+
+					Expect(resp.Products).To(HaveLen(2))
+				})
+
+				Context("Invalid json format", func() {
+					BeforeEach(func() {
+						fakeServerTokenClient = &clientfakes.FakeClient{}
+						fakeServerTokenClient.GetReturns(strings.NewReader("NOT VALID JSON"), nil)
+
+						client = Products{
+							ServerTokenClient: fakeServerTokenClient,
+						}
+					})
+
+					It("Should return an error", func() {
+						_, err := client.ListProducts("321", "123")
+
+						Expect(err).To(HaveOccurred())
+						Expect(err).To(MatchError("Unable to parse Products response from uber: invalid character 'N' looking for beginning of value"))
+					})
+				})
 			})
 
-			It("should return error message returned by uber", func() {
-				_, err := client.GetProduct("321")
+			Context("Uber service returns non-successful response", func() {
+				var uberError error
 
-				Expect(err).To(HaveOccurred())
-				Expect(err).To(MatchError("Unable to get Product Info from uber: Some random error occurred!"))
+				BeforeEach(func() {
+					uberError = fmt.Errorf("Some random error occurred!")
+
+					fakeServerTokenClient = &clientfakes.FakeClient{}
+					fakeServerTokenClient.GetReturns(nil, uberError)
+
+					client = Products{
+						ServerTokenClient: fakeServerTokenClient,
+					}
+				})
+
+				It("should return error message returned by uber", func() {
+					_, err := client.ListProducts("321", "123")
+
+					Expect(err).To(HaveOccurred())
+
+					fmt.Println(err.Error())
+					Expect(err).To(MatchError("Unable to get Products from uber: Some random error occurred!"))
+				})
 			})
-		})
 
+		})
 	})
 })
